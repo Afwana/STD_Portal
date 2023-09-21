9fd8d67b7482904a5ab87342b00fee4a6c14046c
Afwana <afwanakp@gmail.com>
2023-09-21 22:24:33 +0530

    student portal created

---
+++ index.js
@@ -0,0 +1,16 @@
+const jsonServer = require ('json-server')
+
+const server = jsonServer.create()
+
+const middleware = jsonServer.defaults()
+
+const router = jsonServer.router("db.json")
+
+const port = 3200 || process.env.PORT
+
+server.use(middleware)
+server.use(router)
+
+server.listen(port,()=>{
+    console.log("Employee portal server started at port No : " +port);
+})
